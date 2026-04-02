f196261d9a6d1c6a6263f81da21039b93c26f5bf
Juan Esteban Lozano <juan.lcardenas@mail.escuelaing.edu.co>
2026-04-01 21:51:10 -0500

    R4

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 //    Local:      edita .env
 //    Produccion: edita .env.production
 //
-//  Back en Render: https://sportstreamlive-back-1.onrender.com este es el back funcional
+//  Back en Render: https://sportstreamlive-back-1.onrender.com este es el back funcional r4
 // ─────────────────────────────────────────────────────────────
 
 export const API_URL = process.env.REACT_APP_API_URL || 'https://sportstreamlive-back-1.onrender.com';
